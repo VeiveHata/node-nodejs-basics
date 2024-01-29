b6d71cd56bd0348c778c934b248988989be78ed7
Volha Chamlai <volha.chamlai@gmail.com>
2024-01-29 22:11:38 +0100

    01: add hash update

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import crypto, { createHmac } from "crypto";
+import fs from "fs";
+import { stdout } from "process";
+
+const filePath = "src/hash/files/fileToCalculateHashFor.txt";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = await crypto.createHash("sha256");
+  const stream = await fs.createReadStream(filePath);
+
+  stream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on("end", () => {
+    const hashValue = hash.digest("hex");
+    console.log(hashValue);
+  });
+
+  stream.on("error", (error) => {
+    console.error(error);
+  });
 };
 
 await calculateHash();
