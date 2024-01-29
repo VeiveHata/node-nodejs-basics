79a4cdf68aedb2109dd4af5276e05de51da00a01
Volha Chamlai <volha.chamlai@gmail.com>
2024-01-29 22:58:30 +0100

    01: add zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream";
+import { createGzip } from "zlib";
+
+const fileToCompressPath = "src/zip/files/fileToCompress.txt";
+const compressedFilePath = "src/zip/files/archive.gz";
+
 const compress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(fileToCompressPath);
+  const gzipStream = createGzip();
+  const writeStream = createWriteStream(compressedFilePath);
+
+  pipeline(readStream, gzipStream, writeStream, (err) => {
+    if (err) {
+      console.error("An error occurred:", err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream";
+import { createGunzip } from "zlib";
+
+const fileToDeompressPath = "src/zip/files/archive.gz";
+const decomressedFilePath = "src/zip/files/fileToCompress.txt";
+
 const decompress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(fileToDeompressPath);
+  const gunzipStream = createGunzip();
+  const writeStream = createWriteStream(decomressedFilePath);
+
+  pipeline(readStream, gunzipStream, writeStream, (err) => {
+    if (err) {
+      console.error("An error occurred:", err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await decompress();
